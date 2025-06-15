cb5d1d628921ad98432ce648fabf5f1864bfe16d
Rodrigo Perdomo <roperdomo047@gmail.com>
2025-06-15 09:42:58 -0300

    hola

--- demo/src/main/java/com/example/algoritmos_para_el_parcial.java
+++ demo/src/main/java/com/example/algoritmos_para_el_parcial.java
@@ -10,7 +10,11 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
     // DFS - Búsqueda en profundidad
 // Esta función recorre el grafo empezando desde un nodo, y va tan profundo como puede antes de retroceder
